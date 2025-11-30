abfa78a93ab868ed8c5c2ec157bdc6397a1471d1
samuelgautama <maniksamuel117107091@gmail.com>
2025-12-01 02:28:03 +0700

    First Commit

--- lib/screen/profile.dart
+++ lib/screen/profile.dart
@@ -60,7 +60,7 @@ class ProfileScreen extends StatelessWidget{
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(50),
                       ),
-                      child: Image.asset('assets/images/pfp.jpeg'),
+                      child: Image.asset('assets/images/pp.png'),
                     ),
                     Text(
                       'Samuel Gautama Manik',
